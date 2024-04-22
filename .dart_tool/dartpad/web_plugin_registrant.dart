0d5c9d5ff4c5eb85c2a1286f1b5569daa9b59cbf
xBoy-09 <24100300@lums.edu.pk>
2024-04-22 23:38:31 +0500

    Additional files added

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,28 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:audioplayers_web/audioplayers_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:record_web/record_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AudioplayersPlugin.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FlutterKeyboardVisibilityPlugin.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
+  RecordPluginWeb.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
